93b2644a947283f8c56738805168638b2431fa1e
sandaru <sandaru.sahan@gmail.com>
2023-03-09 14:25:54 +0000

    first commit

---
+++ Book_recommendation_system.pl
@@ -0,0 +1,347 @@
+book(1,'Da Vinci Code,The',author('Brown, Dan'),genre(crime_thriller_adventure)).
+book(2,'Harry Potter and the Deathly Hallows',author('Rowling, J.K.'),genre(childrens_fiction)).
+book(3,'Harry Potter and the Philosopher\'s Stone',author('Rowling, J.K.'),genre(childrens_fiction)).
+book(4,'Harry Potter and the Order of the Phoenix',author('Rowling, J.K.'),genre(childrens_fiction)).
+book(5,'Fifty Shades of Grey',author('James, E. L.'),genre(romance_sagas)).
+book(6,'Harry Potter and the Goblet of Fire',author('Rowling, J.K.'),genre(childrens_fiction)).
+book(7,'Harry Potter and the Chamber of Secrets',author('Rowling, J.K.'),genre(childrens_fiction)).
+book(8,'Harry Potter and the Prisoner of Azkaban',author('Rowling, J.K.'),genre(childrens_fiction)).
+book(9,'Angels and Demons',author('Brown, Dan'),genre(crime_thriller_adventure)).
+book(10,'Harry Potter and the Half-blood Prince:Children\'s Edition',author('Rowling, J.K.'),genre(childrens_fiction)).
+book(11,'Fifty Shades Darker',author('James, E. L.'),genre(romance_sagas)).
+book(12,'Twilight',author('Meyer, Stephenie'),genre(young_adult_fiction)).
+book(13,'Girl with the Dragon Tattoo,The:Millennium Trilogy',author('Larsson, Stieg'),genre(crime_thriller_adventure)).
+book(14,'Fifty Shades Freed',author('James, E. L.'),genre(romance_sagas)).
+book(15,'Lost Symbol,The',author('Brown, Dan'),genre(crime_thriller_adventure)).
+book(16,'New Moon',author('Meyer, Stephenie'),genre(young_adult_fiction)).
+book(17,'Deception Point',author('Brown, Dan'),genre(crime_thriller_adventure)).
+book(18,'Eclipse',author('Meyer, Stephenie'),genre(young_adult_fiction)).
+book(19,'Lovely Bones,The',author('Sebold, Alice'),genre(general_literary_fiction)).
+book(20,'Curious Incident of the Dog in the Night-time,The',author('Haddon, Mark'),genre(general_literary_fiction)).
+book(21,'Digital Fortress',author('Brown, Dan'),genre(crime_thriller_adventure)).
+book(22,'Short History of Nearly Everything,A',author('Bryson, Bill'),genre(popular_science)).
+book(23,'Girl Who Played with Fire,The:Millennium Trilogy',author('Larsson, Stieg'),genre(crime_thriller_adventure)).
+book(24,'Breaking Dawn',author('Meyer, Stephenie'),genre(young_adult_fiction)).
+book(25,'Very Hungry Caterpillar,The:The Very Hungry Caterpillar',author('Carle, Eric'),genre(picture_books)).
+book(26,'Gruffalo,The',author('Donaldson, Julia'),genre(picture_books)).
+book(27,'Jamie\'s 30-Minute Meals',author('Oliver, Jamie'),genre(food_drink_general)).
+book(28,'Kite Runner,The',author('Hosseini, Khaled'),genre(general_literary_fiction)).
+book(29,'One Day',author('Nicholls, David'),genre(general_literary_fiction)).
+book(30,'Thousand Splendid Suns,A',author('Hosseini, Khaled'),genre(general_literary_fiction)).
+book(31,'Girl Who Kicked the Hornets\' Nest,The:Millennium Trilogy',author('Larsson, Stieg'),genre(crime_thriller_adventure)).
+book(32,'Time Traveler\'s Wife,The',author('Niffenegger, Audrey'),genre(general_literary_fiction)).
+book(33,'Atonement',author('McEwan, Ian'),genre(general_literary_fiction)).
+book(34,'Bridget Jones\'s Diary:A Novel',author('Fielding, Helen'),genre(general_literary_fiction)).
+book(35,'World According to Clarkson,The',author('Clarkson, Jeremy'),genre(humour_collections_general)).
+book(36,'Captain Corelli\'s Mandolin',author('Bernieres, Louis de'),genre(general_literary_fiction)).
+book(37,'Sound of Laughter,The',author('Kay, Peter'),genre(autobiography_general)).
+book(38,'Life of Pi',author('Martel, Yann'),genre(general_literary_fiction)).
+book(39,'Billy Connolly',author('Stephenson, Pamela'),genre(biography_the_arts)).
+book(40,'Child Called It,A',author('Pelzer, Dave'),genre(autobiography_general)).
+book(41,'Gruffalo\'s Child,The',author('Donaldson, Julia'),genre(picture_books)).
+book(42,'Angela\'s Ashes:A Memoir of a Childhood',author('McCourt, Frank'),genre(autobiography_general)).
+book(43,'Birdsong',author('Faulks, Sebastian'),genre(general_literary_fiction)).
+book(44,'Northern Lights:His Dark Materials S.',author('Pullman, Philip'),genre(young_adult_fiction)).
+book(45,'Labyrinth',author('Mosse, Kate'),genre(general_literary_fiction)).
+book(46,'Harry Potter and the Half-blood Prince',author('Rowling, J.K.'),genre(science_fiction_fantasy)).
+book(47,'Help,The',author('Stockett, Kathryn'),genre(general_literary_fiction)).
+book(48,'Man and Boy',author('Parsons, Tony'),genre(general_literary_fiction)).
+book(49,'Memoirs of a Geisha',author('Golden, Arthur'),genre(general_literary_fiction)).
+book(50,'No.1 Ladies\' Detective Agency,The:No.1 Ladies\' Detective Agency S.',author('McCall Smith, Alexander'),genre(crime_thriller_adventure)).
+book(51,'Island,The',author('Hislop, Victoria'),genre(general_literary_fiction)).
+book(52,'PS, I Love You',author('Ahern, Cecelia'),genre(general_literary_fiction)).
+book(53,'You are What You Eat:The Plan That Will Change Your Life',author('McKeith, Gillian'),genre(fitness_diet)).
+book(54,'Shadow of the Wind,The',author('Zafon, Carlos Ruiz'),genre(general_literary_fiction)).
+book(55,'Tales of Beedle the Bard,The',author('Rowling, J.K.'),genre(childrens_fiction)).
+book(56,'Broker,The',author('Grisham, John'),genre(crime_thriller_adventure)).
+book(57,'Dr. Atkins\' New Diet Revolution:The No-hunger, Luxurious Weight Loss P',author('Atkins, Robert C.'),genre(fitness_diet)).
+book(58,'Subtle Knife,The:His Dark Materials S.',author('Pullman, Philip'),genre(young_adult_fiction)).
+book(59,'Eats, Shoots and Leaves:The Zero Tolerance Approach to Punctuation',author('Truss, Lynne'),genre(usage_writing_guides)).
+book(60,'Delia\'s How to Cook:(Bk.1)',author('Smith, Delia'),genre(food_drink_general)).
+book(61,'Chocolat',author('Harris, Joanne'),genre(general_literary_fiction)).
+book(62,'Boy in the Striped Pyjamas,The',author('Boyne, John'),genre(young_adult_fiction)).
+book(63,'My Sister\'s Keeper',author('Picoult, Jodi'),genre(general_literary_fiction)).
+book(64,'Amber Spyglass,The:His Dark Materials S.',author('Pullman, Philip'),genre(young_adult_fiction)).
+book(65,'To Kill a Mockingbird',author('Lee, Harper'),genre(general_literary_fiction)).
+book(66,'Men are from Mars, Women are from Venus:A Practical Guide for Improvin',author('Gray, John'),genre(popular_culture_media_general_interest)).
+book(67,'Dear Fatty',author('French, Dawn'),genre(autobiography_the_arts)).
+book(68,'Short History of Tractors in Ukrainian,A',author('Lewycka, Marina'),genre(general_literary_fiction)).
+book(69,'Hannibal',author('Harris, Thomas'),genre(crime_thriller_adventure)).
+book(70,'Lord of the Rings,The',author('Tolkien, J. R. R.'),genre(science_fiction_fantasy)).
+book(71,'Stupid White Men:...and Other Sorry Excuses for the State of the Natio',author('Moore, Michael'),genre(current_affairs_issues)).
+book(72,'Interpretation of Murder,The',author('Rubenfeld, Jed'),genre(crime_thriller_adventure)).
+book(73,'Sharon Osbourne Extreme:My Autobiography',author('Osbourne, Sharon'),genre(autobiography_the_arts)).
+book(74,'Alchemist,The:A Fable About Following Your Dream',author('Coelho, Paulo'),genre(general_literary_fiction)).
+book(75,'At My Mother\'s Knee and Other Low Joints',author('O\'Grady, Paul'),genre(autobiography_the_arts)).
+book(76,'Notes from a Small Island',author('Bryson, Bill'),genre(travel_writing)).
+book(77,'Return of the Naked Chef,The',author('Oliver, Jamie'),genre(food_drink_general)).
+book(78,'Bridget Jones: The Edge of Reason',author('Fielding, Helen'),genre(general_literary_fiction)).
+book(79,'Jamie\'s Italy',author('Oliver, Jamie'),genre(food_drink_general)).
+book(80,'I Can Make You Thin',author('McKenna, Paul'),genre(fitness_diet)).
+book(81,'Down Under',author('Bryson, Bill'),genre(travel_writing)).
+book(82,'Summons,The',author('Grisham, John'),genre(crime_thriller_adventure)).
+book(83,'Small Island',author('Levy, Andrea'),genre(general_literary_fiction)).
+book(84,'Nigella Express',author('Lawson, Nigella'),genre(food_drink_general)).
+book(85,'Brick Lane',author('Ali, Monica'),genre(general_literary_fiction)).
+book(86,'Memory Keeper\'s Daughter,The',author('Edwards, Kim'),genre(general_literary_fiction)).
+book(87,'Room on the Broom',author('Donaldson, Julia'),genre(picture_books)).
+book(88,'About a Boy',author('Hornby, Nick'),genre(general_literary_fiction)).
+book(89,'My Booky Wook',author('Brand, Russell'),genre(autobiography_the_arts)).
+book(90,'God Delusion,The',author('Dawkins, Richard'),genre(popular_science)).
+book(91,'"Beano" Annual,The',author('Thompson, D C'),genre(children_annuals)).
+book(92,'White Teeth',author('Smith, Zadie'),genre(general_literary_fiction)).
+book(93,'House at Riverton,The',author('Morton, Kate'),genre(general_literary_fiction)).
+book(94,'Book Thief,The',author('Zusak, Markus'),genre(general_literary_fiction)).
+book(95,'Nights of Rain and Stars',author('Binchy, Maeve'),genre(general_literary_fiction)).
+book(96,'Ghost,The',author('Harris, Robert'),genre(general_literary_fiction)).
+book(97,'Happy Days with the Naked Chef',author('Oliver, Jamie'),genre(food_drink_general)).
+book(98,'Hunger Games,The:Hunger Games Trilogy',author('Collins, Suzanne'),genre(young_adult_fiction)).
+book(99,'Lost Boy,The:A Foster Child\'s Search for the Love of a Family',author('Pelzer, Dave'),genre(biography_general)).
+book(100,'Jamie\'s Ministry of Food:Anyone Can Learn to Cook in 24 Hours',author('Oliver, Jamie'),genre(food_drink_general)).
+
+
+
+author(1,'Brown, Dan', crime_thriller_adventure).
+author(2,'Rowling, J.K.', childrens_fiction).
+author(3,'James, E. L.', romance_sagas).
+author(4,'Meyer, Stephenie', young_adult_fiction).
+author(5,'Larsson, Stieg', crime_thriller_adventure).
+author(6,'Sebold, Alice', general_literary_fiction).
+author(7,'Haddon, Mark', general_literary_fiction).
+author(8,'Bryson, Bill', travel_writing).
+author(9,'Carle, Eric', picture_books).
+author(10,'Donaldson, Julia', picture_books).
+author(11,'Oliver, Jamie', food_drink_general).
+author(12,'Hosseini, Khaled', general_literary_fiction).
+author(13,'Nicholls, David', general_literary_fiction).
+author(14,'McEwan, Ian', general_literary_fiction).
+author(15,'Fielding, Helen', general_literary_fiction).
+author(16,'Clarkson, Jeremy', humour_collections_general).
+author(17,'Bernieres, Louis de', general_literary_fiction).
+author(18,'Kay, Peter', autobiography_general).
+author(19,'Martel, Yann', general_literary_fiction).
+author(20,'Stephenson, Pamela', biography_the_arts).
+author(21,'Pelzer, Dave', biography_general).
+author(22,'McCourt, Frank', autobiography_general).
+author(23,'Faulks, Sebastian', general_literary_fiction).
+author(24,'Pullman, Philip', young_adult_fiction).
+author(25,'Mosse, Kate', general_literary_fiction).
+author(26,'Stockett, Kathryn', general_literary_fiction).
+author(27,'Parsons, Tony', general_literary_fiction).
+author(28,'Golden, Arthur', general_literary_fiction).
+author(29,'McCall Smith, Alexander', crime_thriller_adventure).
+author(30,'Hislop, Victoria', general_literary_fiction).
+author(31,'Ahern, Cecelia', general_literary_fiction).
+author(32,'McKeith, Gillian', fitness_diet).
+author(33,'Zafon, Carlos Ruiz', general_literary_fiction).
+author(34,'Grisham, John', crime_thriller_adventure).
+author(35,'Atkins, Robert C.', fitness_diet).
+author(36,'Truss, Lynne', usage_writing_guides).
+author(37,'Smith, Delia', food_drink_general).
+author(38,'Harris, Joanne', general_literary_fiction).
+author(39,'Boyne, John', young_adult_fiction).
+author(40,'Picoult, Jodi', general_literary_fiction).
+author(41,'Lee, Harper', general_literary_fiction).
+author(42,'Gray, John', popular_culture_media_general_interest).
+author(43,'French, Dawn', autobiography_the_arts).
+author(44,'Lewycka, Marina', general_literary_fiction).
+author(45,'Harris, Thomas', crime_thriller_adventure).
+author(46,'Tolkien, J. R. R.', science_fiction_fantasy).
+author(47,'Moore, Michael', current_affairs_issues).
+author(48,'Rubenfeld, Jed', crime_thriller_adventure).
+author(49,'Osbourne, Sharon', autobiography_the_arts).
+author(50,'Coelho, Paulo', general_literary_fiction).
+author(51,'O\'Grady, Paul', autobiography_the_arts).
+author(52,'McKenna, Paul', fitness_diet).
+author(53,'Levy, Andrea', general_literary_fiction).
+author(54,'Lawson, Nigella', food_drink_general).
+author(55,'Ali, Monica', general_literary_fiction).
+author(56,'Edwards, Kim', general_literary_fiction).
+author(57,'Hornby, Nick', general_literary_fiction).
+author(58,'Brand, Russell', autobiography_the_arts).
+author(59,'Dawkins, Richard', popular_science).
+author(60,'Smith, Zadie', general_literary_fiction).
+author(61,'Morton, Kate', general_literary_fiction).
+author(62,'Zusak, Markus', general_literary_fiction).
+author(63,'Binchy, Maeve', general_literary_fiction).
+author(64,'Harris, Robert', general_literary_fiction).
+author(65,'Collins, Suzanne', young_adult_fiction).
+author(66,'Thompson, D C', children_annuals).
+author(0,'No Preference', n_a).
+
+genere(1,crime_thriller_adventure).
+genere(2,childrens_fiction).
+genere(3,romance_sagas).
+genere(4,young_adult_fiction).
+genere(5,general_literary_fiction).
+genere(6,popular_science).
+genere(7,picture_books).
+genere(8,food_drink_general).
+genere(9,humour_collections_general).
+genere(10,autobiography_general).
+genere(11,autobiography_the_arts).
+genere(12,biography_the_arts).
+genere(13,biography_general).
+genere(14,science_fiction_fantasy).
+genere(15,fitness_diet).
+genere(16,usage_writing_guides).
+genere(17,popular_culture_media_general_interest).
+genere(18,current_affairs_issues).
+genere(19,travel_writing).
+genere(20,children_annuals).
+genere(0,no_preference).
+
+
+/*start Rule*/
+start:-
+    write('*** Book Recommendation System ***'),nl,nl,
+    write('** This system recommends books based on the user selections or preferences. **'),nl,
+    menu.
+
+/*Main menu rule*/
+menu:- 
+    nl,nl,
+    write('Are you in the mood to read a book? "Select 1 or 2"'),nl,
+    write('1- Yes'),nl,
+    write('2- No'),nl,
+    write('Enter your choice:'),nl,
+read(X),
+option(X).
+
+/*second menu rule*/
+menutwo:-
+nl,nl,
+write('Do you want another recommendation? "Select 1 or 2"'),nl,
+write('1- Yes'),nl,
+write('2- No'),nl,
+write('Enter your choice:'),nl,
+read(X),
+option(X).
+
+
+/*menu option rule*/
+option(1):-
+    nl,
+	write('How old are you?'),nl,
+    write('Enter your choice:'),nl,
+	read(Age),
+    nl,
+	write('What is your sex?'),nl,
+    write('Enter your choice:'),nl,
+	read(Sex),
+    nl,
+	write('How you feeling now?'),nl,
+    write('happy'),nl,
+	write('sad'),nl,
+	write('normal'),nl,
+    write('Enter your choice:'),nl,
+	read(Mood),
+		select_genere(Sex, Mood, Age, PrefGenere),
+    	select_author(PrefAuthor, PrefGenere),
+    	nl,write('Your choices : '),write(PrefGenere + PrefAuthor),nl,
+    recommend_book(Sex, Mood, Age, PrefGenere, PrefAuthor).
+
+
+option(2):-
+        write('Thank you, Have a nice day !'),!.
+
+select_genere(Sex, Mood, Age, PrefGenere):-
+    nl,
+    write(' ***Check the recommended generes besed on your inputs***'),nl,nl,
+    write(' What genere of a book are you feeling like reading ? "Select a number from below (Enter 99 to see all genres)"'),nl,nl,
+    write_recommend_gen(Sex, Mood, Age),     
+        write('Enter your choice:'),
+        nl,read(X),
+		handle_genere_choice(X, PrefGenere),
+    	write('Your choice is: '),write(PrefGenere),nl,nl.
+
+select_author(PrefAuthor, PrefGenere):-
+    write(' Who\'s your prefered author ? Select a number from below or enter "0" for no preference'),nl,nl,   
+    write_authors_list(PrefGenere),
+         write('Enter your choice:'),
+         nl,read(Y),
+    	author(Y, PrefAuthor, _),nl,nl,
+    	write('Your choice is: '),write(PrefAuthor),nl.
+
+handle_genere_choice(Choice, PrefG) :-
+    ( Choice == 99 ->  
+    	write_genere_list,
+        read(Z),
+        genere(Z, PrefG),nl,
+    	write('Your choice is: '),write(PrefG),nl,nl
+    );( Choice \= 99 ->
+    	genere(Choice, PrefG)).
+
+write_genere_list :-
+    genere(GenId, Genere),
+    write(GenId),write("."),write(Genere), nl,
+    fail.
+write_genere_list.
+
+write_authors_list(PrefGen) :-
+        ( PrefGen == no_preference -> 
+    	author(AuId, Author, _),
+        write(AuId),write("."),write(Author), nl,
+    	fail
+    );( PrefGen \= no_preference -> 
+        author(AuId, Author, PrefGen),
+        write(AuId),write("."),write(Author), nl,
+        fail
+      ).
+write_authors_list(_).
+
+write_books_list(BkGen, BkAu) :-
+    book(_,BkName,author(BkAu),genre(BkGen)),
+    write(BkName),write(" by "),write(BkAu), nl,
+    fail.
+write_books_list(_, _).
+
+recommend_book(Sex, Mood, Age, PrefGen, PrefAuth):-
+    ( PrefGen == no_preference, PrefAuth \= 'No Preference' ->  
+    	write("Recommended Books are, "),nl,
+        write_books_list(_, PrefAuth),
+        menutwo
+    );( PrefGen \= no_preference, PrefAuth == 'No Preference' ->
+          write("Recommended Books are, "),nl,
+          write_books_list(PrefGen, _),
+          menutwo
+      );( 
+        PrefGen == no_preference, PrefAuth == 'No Preference' ->
+        	recommend_genre(Sex, Mood, Age, GenereList),
+        	random_member(RandGenId, GenereList),
+            genere(RandGenId, RandGen),
+        	write("Since you have no preference, Suggested genere is "),write(RandGen),nl,  
+        	write("Recommended Books are, "),nl,
+          write_books_list(RandGen, _),
+          menutwo
+        );( 
+        PrefGen \= no_preference, PrefAuth \= 'No Preference' ->
+          write("Recommended Books are, "),nl,
+          write_books_list(PrefGen, PrefAuth),
+          menutwo
+        ).
+
+write_recommend_gen(Sex, Mood, Age):-
+    recommend_genre(Sex, Mood, Age, RecGen),
+    print_s(RecGen).
+    
+print_s([]):-!.
+print_s([H|L]):-
+    genere(H,G),
+    write(H),write("."),write(G),
+    nl,
+    print_s(L).
+
+recommend_genre(Sex, Mood, Age, RecGen) 		:- Sex == 'female', Mood == 'happy', Age > 17, RecGen = [3,4,8,9,15,19].  
+recommend_genre(Sex, Mood, Age, RecGen) 		:- Sex == 'female', Mood == 'sad', Age > 17, RecGen = [3,4,9,18]. 
+recommend_genre(Sex, Mood, Age, RecGen) 		:- Sex == 'female', Mood == 'normal', Age > 17, RecGen = [1,5,6,10,13,15,16,17,18,19].
+recommend_genre(Sex, Mood, Age, RecGen) 		:- Sex == 'male', Mood == 'happy', Age > 17, RecGen = [1,3,4,5,6,9,14,15,19].
+recommend_genre(Sex, Mood, Age, RecGen) 		:- Sex == 'male', Mood == 'sad', Age > 17, RecGen = [3,8,9]. 
+recommend_genre(Sex, Mood, Age, RecGen)			:- Sex == 'male', Mood == 'normal', Age > 17, RecGen = [1,3,5,6,8,10,11,12,13,15,16,17,18,19].
+recommend_genre(Sex, Mood, Age, RecGen) 		:- Sex == 'female', Mood == 'happy', Age < 18, RecGen = [2,7,8,15,19,20]. 
+recommend_genre(Sex, Mood, Age, RecGen) 		:- Sex == 'female', Mood == 'sad', Age < 18, RecGen = [7,8,19,20]. 
+recommend_genre(Sex, Mood, Age, RecGen) 		:- Sex == 'female', Mood == 'normal', Age < 18, RecGen = [2,5,6,7,8,10,12,14,15,19,20].
+recommend_genre(Sex, Mood, Age, RecGen) 		:- Sex == 'male', Mood == 'happy', Age < 18, RecGen = [1,2,5,6,7,8,14,15,19,20].
+recommend_genre(Sex, Mood, Age, RecGen) 		:- Sex == 'male', Mood == 'sad', Age < 18, RecGen = [7,8,19,20]. 
+recommend_genre(Sex, Mood, Age, RecGen)			:- Sex == 'male', Mood == 'normal', Age < 18, RecGen = [1,2,5,6,11,12,13,14,15,16,17,19,20].
+recommend_genre(_, _, _, _).
